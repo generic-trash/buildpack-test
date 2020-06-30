77eedff8c0775a02b24ccff2eec8baf67b619f6f
ArduinoidIOT <rohroexperiment@rohroexperiment.com>
2020-06-30 08:57:54 +0530

    Add static routing to server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+app.use('/static', express.static('static'))
 const port = process.env.PORT || 3000
 
 app.get("/", (req, res) =>
-    res.send("<h1>The sample webpage is from https://freewebsitetemplates.com/forums/threads/zerotype-a-blank-canvas-template.20846/ </h1> The page is at /static/index.html"))
+    res.send("<h1>The sample webpage is from https://freewebsitetemplates.com/forums/threads/zerotype-a-blank-canvas-template.20846/ </h1> The page is <a href='/static/index.html'> here </a>"))
 
 app.listen(port)
